6b845af4746efbf10610c791ea53f7b06c2f0688
Siegfried Gevatter <siegfried.gevatter@pal-robotics.com>
2014-02-11 14:07:03 +0000

    Add `wait' parameter for blocking part

--- src/tf_lookup/tf_stream_client.py
+++ src/tf_lookup/tf_stream_client.py
@@ -12,7 +12,11 @@ class TfStreamClient:
         self.sub_id = None
         self.last_update = 0
 
-    def add_transform(self, target, source, cb):
+    def add_transform(self, target, source, cb, wait=False):
+        """
+        If wait is true, this function may block until the action client
+        has connected.
+        """
         target = TLC.sanitize_frame(target)
         source = TLC.sanitize_frame(source)
         key = TLC.key_from_transform(target, source)
@@ -21,18 +25,22 @@ class TfStreamClient:
                 return
         self.transforms[key] = {'cb': cb, 'sub': TLM.Subscription(target, source)}
         self.last_update = rospy.get_time()
-        self._update_transforms()
+        self._update_transforms(wait=wait)
 
-    def _update_transforms(self):
+    def _update_transforms(self, wait):
         goal = TLM.TfStreamGoal()
         if self.sub_id is not None:
             goal.update = True
             goal.subscription_id = self.sub_id
         goal.transforms = [x['sub'] for x in self.transforms.itervalues()]
-        # We can't use a timeout here because of github.com/ros/actionlib/issues/19
-        self.al_client.wait_for_server()
-        #if not self.al_client.wait_for_server(timeout=rospy.Duration(10)):
-        #    raise IOError('Could not connect to tf_stream server.')
+        if wait:
+            # We can't use a timeout here because of github.com/ros/actionlib/issues/19
+            self.al_client.wait_for_server()
+            #if not self.al_client.wait_for_server(timeout=rospy.Duration(10)):
+            #    raise IOError('Could not connect to tf_stream server.')
+            # TODO: ideally we wouldn't block here at all, but instead add a timeout if
+            #       the goal couldn't be sent. It should also take care of re-connecting
+            #       if the action client is restarted.
         self.al_client.send_goal(goal, done_cb=self._al_cb)
 
     def _al_cb(self, status, result):
